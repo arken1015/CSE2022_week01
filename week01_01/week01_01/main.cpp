df3478c09a72aee3e7b13a5b0e8ddf083ba2ba35
arken1015 <arken1015@gmail.com>
2016-09-07 14:11:24 +0900

    stream plus

--- week01_01/week01_01/main.cpp
+++ week01_01/week01_01/main.cpp
@@ -1,7 +1,46 @@
 //first function
 #include <iostream>
+#include <fstream>
+
+class my_variables
+{
+public:
+	int a;
+	int b;
+
+	void print()
+	{
+		printf_s("%d %d\n", a, b);
+	}
+};
+
+std::ostream &operator << (std::ostream &stream, my_variables obj)
+{
+	stream << obj.a << " " << obj.b << std::endl;
+
+	return stream;
+}
 
 void main()
 {
-	printf_s("Hello, World!\n");
+	my_variables val;
+
+	val.a = 1;
+	val.b = 2;
+
+	FILE *of = fopen(".\cstyle.txt", "w");
+
+	fprintf_s(of, "%d %d", val.a, val.b);
+
+	fclose(of);
+
+	std::ofstream ofile(".\cppstyle.txt");
+	ofile << val << std::endl;
+
+	//val.print();
+
+	//std::cout << val << std::endl;
+
+	//printf_s("hello, world!\n");
+	//std::cout << "this is c++ style!\n" << std::endl;
 }
